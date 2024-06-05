91d074a499ced8ab7eaaf242d6d4e5785357f5be
Aleksandr Misonizhnik <misonijnik@gmail.com>
2024-06-06 00:31:02 +0200

    feat: Add a lot of function definitions to the GeneralConverter trait

--- smithril-lib/src/generalized.rs
+++ smithril-lib/src/generalized.rs
@@ -22,18 +22,51 @@ impl fmt::Display for SolverResult {
     }
 }
 
-pub trait GeneralConvertor<'a, S, T>
+pub trait GeneralConverter<'a, S, T>
 where
     S: GeneralSort,
     S: 'a,
     T: GeneralTerm,
     T: 'a,
 {
-    fn mk_smt_symbol(&'a self, name: &str, sort: &S) -> T;
     fn assert(&self, term: &T);
-    fn mk_eq(&'a self, term1: &T, term2: &T) -> T;
     fn check_sat(&self) -> SolverResult;
+    fn mk_and(&'a self, term1: &T, term2: &T) -> T;
     fn mk_bv_sort(&'a self, size: u64) -> S;
+    fn mk_bool_sort(&'a self) -> S;
     fn mk_bv_value_uint64(&'a self, sort: &S, val: u64) -> T;
+    fn mk_bvadd(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_bvand(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_bvashr(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_bvlshr(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_bvmul(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_bvnand(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_bvneg(&'a self, term: &T) -> T;
+    fn mk_bvnor(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_bvnot(&'a self, term: &T) -> T;
+    fn mk_bvnxor(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_bvor(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_bvsdiv(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_bvsge(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_bvsgt(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_bvshl(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_bvsle(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_bvslt(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_bvsmod(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_bvsub(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_bvudiv(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_bvuge(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_bvugt(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_bvule(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_bvult(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_bvumod(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_bvxor(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_eq(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_implies(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_neq(&'a self, term1: &T, term2: &T) -> T;
+    fn mk_not(&'a self, term: &T) -> T;
+    fn mk_or(&'a self, term1: &T, term2: &T) -> T;
     fn mk_smt_bool(&'a self, val: bool) -> T;
+    fn mk_smt_symbol(&'a self, name: &str, sort: &S) -> T;
+    fn mk_xor(&'a self, term1: &T, term2: &T) -> T;
 }
