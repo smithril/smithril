8f57bbfd28f0f064beea731ac2ae51af6f1ef122
Aleksandr Misonizhnik <misonijnik@gmail.com>
2024-06-07 23:01:38 +0200

    test: Update tests

--- smithril-lib/src/lib.rs
+++ smithril-lib/src/lib.rs
@@ -5,42 +5,70 @@ mod z3;
 #[cfg(test)]
 mod tests {
     use crate::bitwuzla::BitwuzlaConverter;
-    use crate::generalized::{GeneralConverter, SolverResult};
+    use crate::generalized::{GeneralConverter, GeneralSort, GeneralTerm, SolverResult};
     use crate::z3::Z3Converter;
+
+    fn generalized_sat_works<'a, C, S, T>(converter: &'a C)
+    where
+        C: GeneralConverter<'a, S, T>,
+        S: GeneralSort,
+        S: 'a,
+        T: GeneralTerm,
+        T: 'a,
+    {
+        let sortbv3 = converter.mk_bv_sort(3);
+        let x = converter.mk_smt_symbol("x", &sortbv3);
+        let y = converter.mk_bv_value_uint64(&sortbv3, 2);
+        let t = converter.mk_eq(&x, &y);
+
+        converter.assert(&t);
+
+        let result = converter.check_sat();
+        assert_eq!(SolverResult::Sat, result);
+    }
+
+    fn generalized_unsat_works<'a, C, S, T>(converter: &'a C)
+    where
+        C: GeneralConverter<'a, S, T>,
+        S: GeneralSort,
+        S: 'a,
+        T: GeneralTerm,
+        T: 'a,
+    {
+        let sortbv3 = converter.mk_bv_sort(3);
+        let x = converter.mk_smt_symbol("x", &sortbv3);
+        let y = converter.mk_bv_value_uint64(&sortbv3, 2);
+        let t = converter.mk_eq(&x, &y);
+        let n = converter.mk_not(&t);
+        let u = converter.mk_and(&t, &n);
+
+        converter.assert(&u);
+
+        let result = converter.check_sat();
+        assert_eq!(SolverResult::Unsat, result);
+    }
 
     #[test]
     fn bitwuzla_sat_works() {
         let bc = BitwuzlaConverter::new();
-
-        let sortbv3 = bc.mk_bv_sort(3);
-
-        let x = bc.mk_smt_symbol("x", &sortbv3);
-
-        let y = bc.mk_bv_value_uint64(&sortbv3, 2);
-
-        let t = bc.mk_eq(&x, &y);
-
-        bc.assert(&t);
-
-        let result = bc.check_sat();
-        assert_eq!(SolverResult::Sat, result);
+        generalized_sat_works(&bc);
     }
 
     #[test]
     fn z3_sat_works() {
         let zc = Z3Converter::new();
+        generalized_sat_works(&zc);
+    }
 
-        let sortbv3 = zc.mk_bv_sort(3);
+    #[test]
+    fn bitwuzla_unsat_works() {
+        let bc = BitwuzlaConverter::new();
+        generalized_unsat_works(&bc);
+    }
 
-        let x = zc.mk_smt_symbol("x", &sortbv3);
-
-        let y = zc.mk_bv_value_uint64(&sortbv3, 2);
-
-        let t = zc.mk_eq(&x, &y);
-
-        zc.assert(&t);
-
-        let result = zc.check_sat();
-        assert_eq!(SolverResult::Sat, result);
+    #[test]
+    fn z3_unsat_works() {
+        let zc = Z3Converter::new();
+        generalized_unsat_works(&zc);
     }
 }
