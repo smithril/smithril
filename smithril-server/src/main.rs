f1c6d2d04706ec0adf8afe8205931f5c8177f9c3
ProskurinMikhail <FaithfulStudent@yandex.ru>
2024-07-06 12:12:03 +0300

    wip: improved solver-client comunication

--- smithril-server/src/main.rs
+++ smithril-server/src/main.rs
@@ -1,7 +1,7 @@
 use ipc_channel::ipc::{IpcReceiver, IpcSender};
 use smithril_lib::{
-    converters::{self, Converter},
-    generalized::{GeneralConverter, GeneralSort, GeneralTerm, SolverQuery, SolverResult},
+    converters::{self, ClientMessageType, ServerMessageType},
+    generalized::GeneralSolver,
 };
 use std::env;
 
@@ -11,57 +11,80 @@ fn main() {
     let one_shot_server_name = &args[1];
     let tx = IpcSender::connect(one_shot_server_name.to_string()).unwrap();
 
-    let (server_tx, server_rx) = ipc_channel::ipc::channel::<String>().unwrap();
-    let (client_tx, client_rx) = ipc_channel::ipc::channel::<String>().unwrap();
+    let (server_tx, server_rx) = ipc_channel::ipc::channel::<ClientMessageType>().unwrap();
+    let (client_tx, client_rx) = ipc_channel::ipc::channel::<ServerMessageType>().unwrap();
 
     tx.send((server_tx, client_rx)).unwrap();
 
-    let receiver: IpcReceiver<String> = server_rx;
-    let sender: IpcSender<String> = client_tx;
+    let receiver: IpcReceiver<ClientMessageType> = server_rx;
+    let sender: IpcSender<ServerMessageType> = client_tx;
+    let mut conv: Option<Box<dyn GeneralSolver>> = Option::None;
 
     // Receive the converter setup message
-    let converter_line = receiver.recv().expect("Failed to receive converter");
-    let converter: Converter =
-        serde_json::from_str(&converter_line).expect("Failed to parse converter");
-
-    // Send confirmation of converter setup
-    match converter {
-        Converter::Bitwuzla => {
-            sender
-                .send("Bitwuzla converter has set up".to_string())
-                .unwrap();
-        }
-        Converter::Z3 => {
-            sender.send("Z3 converter has set up".to_string()).unwrap();
-        }
-    }
-
-    // Process each subsequent message
-    while let Ok(input_line) = receiver.recv() {
-        let input: SolverQuery = serde_json::from_str(&input_line).unwrap();
-        let output = match converter {
-            Converter::Bitwuzla => {
-                let bc = converters::mk_bitwulza();
-                solve_problem(&bc, input)
+    while let Ok(cur_line) = receiver.recv() {
+        match cur_line {
+            ClientMessageType::Converter(converter) => match converter {
+                converters::ConverterType::Bitwuzla => {
+                    sender
+                        .send(ServerMessageType::Txt(
+                            "Bitwuzla converter has set up".to_string(),
+                        ))
+                        .unwrap();
+                    conv = Some(Box::new(converters::mk_bitwulza()));
+                }
+                converters::ConverterType::Z3 => {
+                    sender
+                        .send(ServerMessageType::Txt(
+                            "Z3 converter has set up".to_string(),
+                        ))
+                        .unwrap();
+                    conv = Some(Box::new(converters::mk_z3()));
+                }
+            },
+            // ClientMessageType::Query(input_query) => {
+            //     let output: SolverResult = match conv.as_ref() {
+            //         Some(conv) => solve_problem(conv.as_ref(), input_query),
+            //         None => SolverResult::Unknown,
+            //     };
+            //     sender.send(ServerMessageType::Result(output)).unwrap();
+            // }
+            ClientMessageType::Assert(input_query) => {
+                match conv.as_ref() {
+                    Some(conv) => {
+                        conv.assert(&input_query.query);
+                        sender
+                            .send(ServerMessageType::Txt("Assert successful".to_string()))
+                            .unwrap();
+                    }
+                    None => sender
+                        .send(ServerMessageType::Txt(
+                            "Converter is not set up".to_string(),
+                        ))
+                        .unwrap(),
+                };
             }
-            Converter::Z3 => {
-                let bc = converters::mk_z3();
-                solve_problem(&bc, input)
-            }
-        };
-        let output_json = serde_json::to_string(&output).unwrap();
-        sender.send(output_json).unwrap();
+            ClientMessageType::CheckSat() => {
+                match conv.as_ref() {
+                    Some(conv) => {
+                        let output = conv.check_sat();
+                        sender.send(ServerMessageType::Result(output)).unwrap();
+                    }
+                    None => sender
+                        .send(ServerMessageType::Txt(
+                            "Converter is not set up".to_string(),
+                        ))
+                        .unwrap(),
+                };
+            } //mixa117 assert and check
+              //mixa117 test for GeneralSolver in lib.rs
+              //mixa117 evaluate (term) get_model
+              //mixa117 unsat_core
+              //push GeneralSolver+tests (without client/server)
+        }
     }
 }
 
-fn solve_problem<'a, C, S, T>(converter: &'a C, input: SolverQuery) -> SolverResult
-where
-    C: GeneralConverter<'a, S, T>,
-    S: GeneralSort,
-    S: 'a,
-    T: GeneralTerm,
-    T: 'a,
-{
-    converter.assert(&converter.convert_term(&input.query));
-    converter.check_sat()
-}
+// fn solve_problem<'a>(converter: &'a dyn GeneralSolver, input: SolverQuery) -> SolverResult {
+//     converter.assert(&input.query);
+//     converter.check_sat()
+// }
